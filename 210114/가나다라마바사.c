361b7bc91b7cb77ba740f1071b444231526fac4e
0114 <User@BCLASS-11>
2021-01-14 19:19:35 +0900

    2차저장_대입연산 엽습

--- 210114/가나다라마바사.c
+++ 210114/가나다라마바사.c
@@ -2,7 +2,25 @@
 
 int main(void) {
 
+	
+	int age; //변수 선언
+	age = 10; //값 할당
 
+	printf("%d\n\n", age)
+		; //age란 변수에 10이라는 값을 할당한다.
+
+	int age2 = 10; //변수선언과 할당을 동시 진행
+
+	int a = 10;
+	int b = 20;
+	int c = 30;
+
+	int a, b, c;
+	a = 10;
+	b = 20;
+	c = 30;
+
+	int a_1 = 10, b_2 = 20, c_2 = 30;
 
 
 	return 0;
